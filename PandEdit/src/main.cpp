6aaf49464fea03313f3dc1a3fa8ffd2d0c193d9c
Sahil Iyer <iyersahil12@gmail.com>
2020-04-14 11:02:22 +1000

    Added Glad to project.

--- PandEdit/src/main.cpp
+++ PandEdit/src/main.cpp
@@ -1,9 +1,16 @@
 //  ===== Date Created: 14 April, 2020 ===== 
 
 #include <stdio.h>
+#include <glad/glad.h>
 
 int main(int argc, char* argv[])
 {
-	printf("PandEdit.\n");
+	if (!gladLoadGL())
+	{
+		// Will fail due to no context
+		printf("Glad initialisation failed.\n");
+		return 1;
+	}
+	
 	return 0;
 }
