fc1ca5e36b504570ed3a8a39434446c8c8d93d98
Sahil Iyer <iyersahil12@gmail.com>
2020-05-04 09:21:51 +1000

    Modified build files to allow for release builds.

--- build.bat
+++ build.bat
@@ -3,18 +3,39 @@ rem  ===== Date Created: 14 April, 2020 =====
 
 set ROOT_DIRECTORY=%CD%
 
+set NORUN=false
+set CONFIG=Debug
+
+FOR %%A IN (%*) DO (
+	IF [%%A]==[-norun] (
+		set NORUN=true
+	) ELSE IF [%%A]==[-relinfo] (
+		set CONFIG=RelWithDebInfo
+	) ELSE IF [%%A]==[-release] (
+		set CONFIG=Release
+	)
+)
+
 IF NOT EXIST build\ md build
 cd build
 
 cmake .. -G "Visual Studio 16 2019"
 IF %ERRORLEVEL% NEQ 0 GOTO end
-cmake --build .
+cmake --build . --config %CONFIG%
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
 echo.
 cd %ROOT_DIRECTORY%
 
-if NOT "%~1"=="-norun" call run.bat
+IF NOT %NORUN%==true (
+	IF %CONFIG%==Debug (
+		call run.bat
+	) ELSE IF %CONFIG%==RelWithDebInfo (
+		call run.bat -relinfo
+	) ELSE (
+		call run.bat -release
+	)
+)
 
 :end
 cd %ROOT_DIRECTORY%
--- run.bat
+++ run.bat
@@ -4,6 +4,7 @@ rem  ===== Date Created: 14 April, 2020 =====
 set ROOT_DIRECTORY=%CD%
 
 set CONFIG_DIR=Debug
+IF "%~1"=="-relinfo" set CONFIG_DIR=RelWithDebInfo
 IF "%~1"=="-release" set CONFIG_DIR=Release
 
 IF EXIST bin\%CONFIG_DIR%\ (
